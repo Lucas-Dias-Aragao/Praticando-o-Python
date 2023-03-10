85c99ca070601b677f1e8887d0753936163a8d97
Lucas Dias <110422926+Lucas-Dias-Aragao@users.noreply.github.com>
2023-03-10 12:25:12 -0300

    Adiciona arquivos

---
+++ ListaDeExercicios/EstruturaSequencial/Ex7_AreaDoQuadrado.py
@@ -0,0 +1,9 @@
+# Cálculo de área e dobro da área
+
+lado = int(input("Informe um lado do quadrado: "))
+area_quadrado = lado * lado
+dobro_da_area = area_quadrado * 2
+
+print("A área do quadrado é: {}".format(area_quadrado))
+print("O dobro da área do quadrado é: {}".format(dobro_da_area))
+
