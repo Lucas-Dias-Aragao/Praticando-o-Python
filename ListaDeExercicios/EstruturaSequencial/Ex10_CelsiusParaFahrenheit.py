f1e810d4ef39fbc38ae0a76f3b6ccf45d8e2f648
Lucas Dias <110422926+Lucas-Dias-Aragao@users.noreply.github.com>
2023-03-30 16:45:52 -0300

    Delete ListaDeExercicios/EstruturaSequencial directory

--- ListaDeExercicios/EstruturaSequencial/Ex10_CelsiusParaFahrenheit.py
+++
@@ -1,4 +0,0 @@
-graus_celsius = int(input("Temperatura em Celsius: "))
-conversao_para_fahrenheit = (graus_celsius * 1.8) + 32
-
-print(f"Resultado da aprox. da conversão:{conversao_para_fahrenheit: .1f} ºF")
--- ListaDeExercicios/EstruturaSequencial/Ex9_FahrenheitParaCelsius.py
+++
@@ -1,4 +0,0 @@
-graus_fahrenheit = int(input("Temperatura em Fahrenheit: "))
-conversao_para_celsius = 5 * ((graus_fahrenheit - 32) / 9)
-
-print(f"Resultado da aprox. da conversão:{conversao_para_celsius: .1f} ºC")
